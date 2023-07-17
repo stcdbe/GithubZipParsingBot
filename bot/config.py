a05a25b5142ab2252df8ee2e02bfce21481a5a12
stcdbe <samthomascolddeadblueeyes@gmail.com>
2023-07-17 20:23:31 +1000

    Initail commit

--- bot/config.py
+++ bot/config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-APITOKEN = str(os.getenv("API_TOKEN"))
+APITOKEN = str(os.getenv("APITOKEN"))
 
 REDISHOST = str(os.getenv('REDISHOST'))
 REDISPORT = int(os.getenv('REDISPORT'))
